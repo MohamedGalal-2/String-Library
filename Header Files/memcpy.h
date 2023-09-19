b61910cb884a2dfb30226089e0d4d72faf8ee202
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-09-19 16:29:57 +0300

    Added header Files for memcpy and memset

---
+++ Header Files/memcpy.h
@@ -0,0 +1,11 @@
+/*File Guard*/
+#ifndef _MEMCPY_C_
+#define _MEMCPY_C_
+
+/*Include Sections*/
+#include <stdio.h>
+
+/*Function Declaration Section*/
+unsigned int Memcpy(char str1[], char str2[], unsigned int number_of_bytes);
+
+#endif
---
+++ Header Files/memset.h
@@ -0,0 +1,11 @@
+/*File Guard*/
+#ifndef _MEMSET_C_
+#define _MEMSET_C_
+
+/*Include Sections*/
+#include <stdio.h>
+
+/*Function Declaration Section*/
+unsigned int Memset(char str[], unsigned char val, unsigned int number_of_bytes);
+
+#endif
