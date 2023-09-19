8cfb4f288be2b365af4329d7b8295a1e09741bbf
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-09-19 21:26:52 +0300

    Added strncat library header file

---
+++ Header Files/strncat.h
@@ -0,0 +1,11 @@
+/*File Guard*/
+#ifndef _STRNCAT_H
+#define _STRNCAT_H
+
+/*Include Sections*/
+#include <stdio.h>
+
+/*Function Declaration Section*/
+char* my_strncat(char* dest, const char* src, size_t n);
+
+#endif
