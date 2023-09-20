8e6ed555100d4c7e8bb711b55bc3aea98b1fe456
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-09-20 13:06:36 +0300

    Added strcmp function header file

---
+++ Header Files/strcmp.h
@@ -0,0 +1,11 @@
+/*File Guard*/
+#ifndef _STRCMP_H
+#define _STRCMP_H
+
+/*Include Sections*/
+#include <stdio.h>
+
+/*Function Declaration Section*/
+int my_strcmp(const char* str1, const char* str2);
+
+#endif
