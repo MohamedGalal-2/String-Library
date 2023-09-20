913c035be9c891baffad3f71fa63b6e83c045dec
Mohamed Galal <MohamedAhmedGalal74_sd@eng.bsu.edu.eg>
2023-09-20 12:56:24 +0300

    Added strcmp function soruce file

---
+++ Source Files/strcmp.c
@@ -0,0 +1,68 @@
+/*Function Definition Section*/
+#include "..\Header Files\strcmp.h"
+
+/*Function Definition Section*/
+int my_strcmp(const char* str1, const char* str2)
+{
+	int Retval = -1;
+
+	int str1len = 0;
+	int str2len = 0;
+	int strlenIndex = 0;
+	while (str1[strlenIndex] != '\0')
+	{
+		str1len++;
+		strlenIndex++;
+	}
+	strlenIndex = 0;
+	while (str2[strlenIndex] != '\0')
+	{
+		str2len++;
+		strlenIndex++;
+	}
+
+	printf("%d %d\n", str1len, str2len);
+
+	int index = 0;
+	if (str1len > str2len) {
+		index = str1len;
+	}
+	else
+	{
+		index = str2len;
+	}
+
+	if (NULL == (char)str1 || NULL == (char)str2)
+	{
+		Retval = -1;
+	}
+	else
+	{
+		for (int i = 0; i < index; i++)
+		{
+			if (str1[i] == str2[i])
+			{
+				Retval = 0;
+			}
+			else if (str1[i] != str2[i])
+			{
+				if (str2len > str1len)
+				{
+					Retval = -1;
+					break;
+				}
+				else if (str1len > str2len)
+				{
+					Retval = 1;
+					break;
+				}
+			}
+			else
+			{
+				Retval = -1;
+				break;
+			}
+		}
+	}
+	return Retval;
+}
